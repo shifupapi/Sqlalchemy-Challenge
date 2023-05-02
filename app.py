ef80df32668b2a4cf58ef3de8f8e06eced995e37
Shifupapi <shifupapi@Seans-MacBook-Air.local>
2023-05-02 18:00:38 -0400

    code

--- app.py
+++ app.py
@@ -76,27 +76,6 @@ def stations():
     all_stations=list(np.ravel(results))
     session.close()
     return jsonify(all_stations)
-    
-
-@app.route("/api/v1.0/tobs")
-def tempartureobs():
-    #results_date = dt.date(2017,8,23) - dt.timedelta(days=366)
-
-    station_Measurment_join=session.query(Measurement.station,func.count(Measurement.station)).\
-                       group_by(Measurement.station).\
-                       order_by(func.count(Measurement.station).desc()).all()
-    temp=session.query(Measurement.date).order_by(Measurement.date.desc()).\
-             filter(Measurement.station==station_Measurment_join[0][0]).first()
-    str_date=list(np.ravel(temp))[0]
-    latest_date=dt.datetime.strptime(str_date,"%Y-%m-%d")
-    year_back=latest_date-dt.timedelta(days=366)
-
-    results_temp=session.query(Measurement.tobs).\
-             filter(Measurement.station==station_Measurment_join[0][0]).\
-             filter(Measurement.date>=year_back).all()
-
-    session.close()
-    return jsonify(results_temp)
 
 
 #Tobs Route
